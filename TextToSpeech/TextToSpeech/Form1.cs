6a14d098746729d69abb256e7ed4d6153a0b1e9e
acquire <wegham@gmail.com>
2014-07-24 13:25:09 +1000

    First Commit
    Include play and pause functionality.

---
+++ TextToSpeech/TextToSpeech/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        PromptBuilder pbuilder = new PromptBuilder();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pbuilder.ClearContent();
+            pbuilder.AppendText(textBox1.Text);
+            synthesizer.SpeakAsync(pbuilder);
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (synthesizer.State == SynthesizerState.Speaking)
+            {
+                synthesizer.Pause();
+                button2.Text = "Continue";
+            }
+            else if (synthesizer.State == SynthesizerState.Paused)
+            {
+                synthesizer.Resume();
+                button2.Text = "Pause";
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            synthesizer.Dispose();
+
+        }
+
+
+    }
+}
